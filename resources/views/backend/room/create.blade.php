168ecc3c6a54a4fcba1391ff1896c511d1c8d728
= <=>
2020-11-13 09:38:57 +0700

    update_roomCreate

--- resources/views/backend/room/create.blade.php
+++ resources/views/backend/room/create.blade.php
@@ -7,12 +7,12 @@
     </section>
     <section class="content">
         <div class="row">
-            <div class="col-md-6">
-                <div class="box box-primary">
-                    <form role="form" action="{{route('admin.room.store')}}" method="post" enctype="multipart/form-data">
-                        @csrf
+            <form role="form" action="{{route('admin.room.store')}}" method="post" enctype="multipart/form-data">
+                @csrf
+                <div class="col-md-6">
+                    <div class="box box-primary">
                         <div class="box-body">
-{{--                            loại phòng--}}
+    {{--                            loại phòng--}}
                             <div class="form-group">
                                 <label for="exampleInputEmail1">Loại Phòng</label>
                                 <select class="form-control w-50" name="typeRoom">
@@ -22,12 +22,12 @@
                                     @endforeach
                                 </select>
                             </div>
-{{--                            tiêu đề--}}
+    {{--                            tiêu đề--}}
                             <div class="form-group">
                                 <label for="exampleInputEmail1">Tên Tiêu Đề</label>
                                 <input type="text" class="form-control" id="title" name="title" placeholder="Nhập tên tiêu đề">
                             </div>
-{{--                            tỉnh/thành phố--}}
+    {{--                            tỉnh/thành phố--}}
                             <div class="form-group">
                                 <label for="exampleInputEmail1">Tỉnh/Thành Phố</label>
                                 <select class="form-control w-50" name="city">
@@ -37,7 +37,7 @@
                                     @endforeach
                                 </select>
                             </div>
-{{--                            quận huyện--}}
+    {{--                            quận huyện--}}
                             <div class="form-group">
                                 <label for="exampleInputEmail1">Quận/Huyện</label>
                                 <select class="form-control w-50" name="district">
@@ -47,59 +47,59 @@
                                     @endforeach
                                 </select>
                             </div>
-{{--                            địa chỉ--}}
+    {{--                            địa chỉ--}}
                             <div class="form-group">
                                 <label for="exampleInputEmail1">Địa chỉ Cụ Thể</label>
                                 <input type="text" class="form-control" id="address" name="address" placeholder="Address">
                             </div>
-{{--                            số lượng phòng--}}
+    {{--                            số lượng phòng--}}
                             <div class="form-group">
                                 <label for="exampleInputEmail1">Số Lượng Phòng</label>
                                 <input type="text" class="form-control" id="quantity" name="quantity" placeholder="Nhập số lượng">
                             </div>
-{{--                            giá phòng--}}
+    {{--                            giá phòng--}}
                             <div class="form-group">
                                 <label for="exampleInputEmail1">Giá Phòng (vnđ)</label>
                                 <input type="text" class="form-control" id="price" name="price" placeholder="Nhập giá phòng">
                             </div>
-{{--                            ảnh--}}
+    {{--                            ảnh--}}
                             <div class="form-group">
                                 <label for="exampleInputFile">Ảnh Phòng Trọ</label>
                                 <input type="file" class="" id="image" name="image" >
-{{--                                multiple--}}
+    {{--                                multiple--}}
                             </div>
-{{--                            diện tích--}}
+    {{--                            diện tích--}}
                             <div class="form-group">
                                 <label for="exampleInputFile">Diện Tích Phòng</label>
                                 <input type="text" class="form-control" id="area" name="area" placeholder="Nhập diện tích">
                             </div>
-{{--                            ghi chú--}}
+    {{--                            ghi chú--}}
                             <div class="form-group">
                                 <label for="exampleInputFile">Ghi Chú</label>
                                 <input type="text" class="form-control" id="note" name="note" placeholder="Ghi chú">
                             </div>
-{{--                            chung chủ--}}
+    {{--                            chung chủ--}}
                             <div class="form-group">
                                 <label>
                                     <input type="checkbox" value="1" name="owner"> Chung Chủ
                                 </label>
                             </div>
-{{--                            ngày đăng bài--}}
+    {{--                            ngày đăng bài--}}
                             <div class="form-group">
                                 <label for="exampleInputFile">Ngày Đăng Bài</label>
                                 <input type="text" class="form-control" id="publicDate" name="publicDate" placeholder="Ngày đăng bài">
                             </div>
-{{--                            ngày hết hạn--}}
+    {{--                            ngày hết hạn--}}
                             <div class="form-group">
                                 <label for="exampleInputFile">Ngày Hết Hạn</label>
                                 <input type="text" class="form-control" id="expiredDate" name="expiredDate" placeholder="Ngày hết hạn">
                             </div>
-{{--                            giá điện--}}
+    {{--                            giá điện--}}
                             <div class="form-group">
                                 <label for="exampleInputFile">Giá Điện</label>
                                 <input type="text" class="form-control" id="electricPrice" name="electricPrice" placeholder="Nhập giá điện/1 số">
                             </div>
-{{--                            giá nước--}}
+    {{--                            giá nước--}}
                             <div class="form-group">
                                 <label for="exampleInputFile">Giá Nước</label>
                                 <input type="text" class="form-control" id="waterPrice" name="waterPrice" placeholder="giá nước/1 khối">
@@ -109,9 +109,9 @@
                                 <label for="exampleInputEmail1">Id Người Đăng</label>
                                     <select class="form-control w-50" name="userID">
                                         <option value="0">-- Chọn ID Người Đăng  --</option>
-{{--                                             @foreach($categories as $category)--}}
+    {{--                                             @foreach($categories as $category)--}}
                                         <option value="1">le cuong</option>
-{{--                                            @endforeach--}}
+    {{--                                            @endforeach--}}
                                     </select>
                             </div>
 
@@ -119,9 +119,9 @@
                                 <label for="exampleInputEmail1">Id Người Phê Duyệt</label>
                                 <select class="form-control w-50" name="approvalID">
                                     <option value="0">-- Chọn ID Người Phê Duyệt  --</option>
-{{--                                         @foreach($categories as $category)--}}
+    {{--                                         @foreach($categories as $category)--}}
                                     <option value="1">le cuong</option>
-{{--                                        @endforeach--}}
+    {{--                                        @endforeach--}}
                                 </select>
                             </div>
                             <div class="form-group">
@@ -145,9 +145,33 @@
                         <div class="box-footer">
                             <button type="submit" class="btn btn-primary">Tạo</button>
                         </div>
-                    </form>
+                    </div>
                 </div>
-            </div>
+                <div class="col-md-4">
+                    <div class="box box-primary">
+                        <div class="box-header with-border">
+                            <h3 class="box-title">Chi tiết ảnh sản phẩm</h3>
+                        </div>
+                        <!-- /.box-header -->
+                        <!-- form start -->
+
+                        <div class="box-body">
+                            @for($i=1; $i<=5; $i++)
+                                <div class="form-group">
+                                    <label for="exampleInputFile">Ảnh sản phẩm {{$i}}</label>
+                                    <input type="file" class="" id="image" name="detailImage{{$i}}">
+                                </div>
+                            @endfor
+                        </div>
+                        <!-- /.box-body -->
+                        <div class="box-footer">
+                            {{--                                <button type="submit" class="btn btn-primary">Tạo</button>--}}
+                            <input type="reset" class="btn btn-default" value="Reset">
+                        </div>
+
+                    </div>
+                </div>
+            </form>
         </div>
     </section>
 @endsection
