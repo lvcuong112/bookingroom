588495ecfc5a677d82fd890f7b9fc08fb3ca56df
= <=>
2020-12-09 01:57:29 +0700

    update

--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -2,24 +2,27 @@
     <div class="container-fluid d-flex justify-content-between align-items-center container-lg">
         <div class="logo d-flex align-item-center">
             <a href="/Home"><img src="https://grandetest.com/theme/findhouse-html/images/header-logo2.png" /></a>
-            <span class="ml-2"> Find House </span>
+            <span class="ml-2"> Thuê Trọ </span>
             <div class="box-search ml-5 d-flex align-item-center">
-                <input type="text" name="search" placeholder="What are you looking for?">
+                <input type="text" name="search" placeholder="Tìm kiếm?">
                 <button> <i class="fas fa-search"></i> </button>
             </div>
         </div>
         <ul class="menu d-none d-lg-flex m-0">
             <li>
-                <a href="index.html"> Trang chủ</a>
+                <a href="/"> Trang chủ</a>
             </li>
             <li>
-                <a href="Rent.html"> Thuê nhà </a>
-            </li>
-            <li>
-                <a href="/"> BLog </a>
+                <a href="/"> Thuê trọ </a>
             </li>
             <li>
                 <a type="button" class="btn-contact"> Liên hệ </a>
+            </li>
+            <li>
+                <a type="button" class="btn-contact"> Đăng nhập </a>
+            </li>
+            <li>
+                <a type="button" class="btn-contact"> Đăng ký </a>
             </li>
         </ul>
         <!-- <button class="btn-menu-moblie d-block d-lg-none">
